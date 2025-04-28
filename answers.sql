fa76cc21c55df277ea5e997d7c1b428807664e0a
Edwin Gichira <114245977+EdiGich@users.noreply.github.com>
2025-04-28 14:20:57 +0300

    Week 7 assignment complete

---
+++ answers.sql
@@ -0,0 +1,74 @@
+--1. 
+-- Write an SQL query to transform the table into 1NF, ensuring that each row represents a single product for an order
+
+--Creating a table ProductDetail_1NF that has three columns, i.e OrderId, CustomerName and Product
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(255),
+    Product VARCHAR(255)
+);
+
+-- Inserting data into the ProductDetail_1NF table with each product in a separate row
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product) VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+--2.
+--In the table above, the CustomerName column depends on OrderID (a partial dependency), which violates 2NF.
+-- Write an SQL query to transform this table into 2NF by removing partial dependencies. Ensure that each non-key column fully depends on the entire primary key.
+
+--2nf can be achieved by removing partial dependencies from the OrderDetails table.
+--Split the table into two tables, Orders table and OrderItems table.
+-- Orders table has got two fields, OrderID and CustomerName.
+-- OrderItems table has got 3 fields, OrderId, Product and Quantity field.
+
+-- Creating the Orders table
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(255)
+);
+
+--Insert OrderID and CustomerName into the Orders table.
+INSERT INTO Orders (OrderID, CustomerName) VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+--The OrderItems table
+CREATE TABLE OrderItems (
+    OrderItemID INT AUTO_INCREMENT PRIMARY KEY, -- OrderItems is the unique identifier hence cannot occur more than once in this OrderItems table.
+    OrderID INT,
+    Product VARCHAR(255),
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID) -- Establish relationship with the Orders Table
+);
+
+--Insert data into OrderItems
+INSERT INTO OrderItems (OrderID, Product, Quantity) VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
+
+--View the individual Orders and OrderItems tables.
+SELECT * FROM Orders;
+SELECT * FROM OrderItems;
+
+-- Joining Orders and OrderItems tables based on the OrderID.  Each product appears with the correct customer.
+SELECT 
+    o.OrderID,
+    o.CustomerName,
+    oi.Product,
+    oi.Quantity
+FROM 
+    Orders AS o
+INNER JOIN 
+    OrderItems AS oi ON o.OrderID = oi.OrderID
+ORDER BY 
+    o.OrderID;
